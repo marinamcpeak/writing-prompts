bf64d992f313131fbe7970323ae5b5a6d2222c35
Marina <mcpeakmarina@gmail.com>
2021-05-21 22:16:51 -0600

    Removed [WP] from title and added banned terms.

--- scrape.js
+++ scrape.js
@@ -14,16 +14,29 @@ const RedditScraper = require("reddit-scraper");
   };
 
   try {
-      const redditScraper = new RedditScraper.RedditScraper(redditScraperOptions);
-      const scrapedData = await redditScraper.scrapeData(requestOptions);
-      const data = scrapedData.map(d => ({
-        title: d.data.title,
-        created: d.data.created,
-        id: d.data.name
-      }));
-      fs.writeFileSync('./prompts.json', JSON.stringify(data));
-      console.log(`Saved ${data.length} records`);
-  } catch (error) {
-      console.error(error);
-  }
+    const redditScraper = new RedditScraper.RedditScraper(redditScraperOptions);
+    const scrapedData = await redditScraper.scrapeData(requestOptions);
+    let data = scrapedData.map(d => ({
+      title: d.data.title,
+      created: d.data.created,
+      id: d.data.name
+    }));
+    
+    data = data.filter(d => d.title.startsWith('[WP]'))
+    
+    bannedTerms.forEach(term => {
+      data = data.filter(d => !d.title.includes(term));
+    });
+    
+    data.forEach(d => {
+      d.title = d.title.replace('[WP]:', '');
+      d.title = d.title.replace('[WP]', '');
+      d.title = d.title.trim();
+    });
+
+    fs.writeFileSync('./prompts.json', JSON.stringify(data));
+    console.log(`Saved ${data.length} records`);
+} catch (error) {
+    console.error(error);
+}
 })();
